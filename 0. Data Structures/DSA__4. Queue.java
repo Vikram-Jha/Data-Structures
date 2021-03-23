b943455a79dd287c48d3cce53b99016632848b62
Vikram Jha <77636318+Vikram-Jha@users.noreply.github.com>
2021-03-23 17:03:20 +0530

    Update DSA__4. Queue.java

--- 0. Data Structures/DSA__4. Queue.java
+++ 0. Data Structures/DSA__4. Queue.java
@@ -8,9 +8,13 @@
 
         while (!queue.isEmpty())
             stack.push(queue.remove());
+  
         while (!stack.isEmpty())
             queue.add(stack.pop());
     }
+
+//(A queue can be implemenated using array linklist and stack we need to know every implementation)
+
 
 //Normal Queue implementation using array
 
@@ -64,5 +68,60 @@ public class ArrayQueue {
 }
 
 //Circular queue implementation using arrays
+package com.codewithmosh;
+
+import java.util.Arrays;
+
+public class ArrayQueue {
+  private int[] items;
+  private int rear;
+  private int front;
+  private int count;
+
+  public ArrayQueue(int capacity) {
+    items = new int[capacity];
+  }
+
+  public void enqueue(int item) {
+    if (isFull())
+      throw new IllegalStateException();
+
+    items[rear] = item;
+    rear = (rear + 1) % items.length;
+    count++;
+  }
+
+  public int dequeue() {
+    if (isEmpty())
+      throw new IllegalStateException();
+
+    var item = items[front];
+    items[front] = 0;
+    front = (front + 1) % items.length;
+    count--;
+
+    return item;
+  }
+
+  public int peek() {
+    if (isEmpty())
+      throw new IllegalStateException();
+
+    return items[front];
+  }
+
+  public boolean isEmpty() {
+    return count == 0;
+  }
+
+  public boolean isFull() {
+    return count == items.length;
+  }
+
+  @Override
+  public String toString() {
+    return Arrays.toString(items);
+  }
+}
 
 
