8186977d669d0afd9bafeafb62e383b3b4b20d0a
Vikram Jha <77636318+Vikram-Jha@users.noreply.github.com>
2021-03-23 16:51:02 +0530

    Update DSA__4. Queue.java

--- 0. Data Structures/DSA__4. Queue.java
+++ 0. Data Structures/DSA__4. Queue.java
@@ -11,3 +11,58 @@
         while (!stack.isEmpty())
             queue.add(stack.pop());
     }
+
+//Normal Queue implementation using array
+
+package com.company;
+
+public class ArrayQueue {
+    private int[] items;
+    private int rear;
+    private int front;
+
+    public ArrayQueue(int capacity) {
+        items = new int[capacity];
+    }
+
+    public void enqueue(int item) {
+        if (isFull())
+            throw new IllegalStateException("Queue is full");
+
+        items[rear++] = item;
+    }
+
+    public int dequeue() {
+        if (isEmpty())
+            throw new IllegalStateException("Queue is empty");
+
+        var item = items[front];
+        items[front++] = 0;
+
+        return item;
+    }
+
+    public int peek() {
+        if (isEmpty())
+            throw new IllegalStateException("Queue is empty");
+
+        return items[front];
+    }
+
+    public boolean isEmpty() {
+        return front== rear;
+    }
+
+    public boolean isFull() {
+        return rear == items.length;
+    }
+
+    @Override
+    public String toString() {
+        return java.util.Arrays.toString(items);
+    }
+}
+
+//Circular queue implementation using arrays
+
+
