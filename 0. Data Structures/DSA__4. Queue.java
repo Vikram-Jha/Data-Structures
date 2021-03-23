b692ba221cc4932cc70882c0b83aca7fd95e33ca
Vikram Jha <77636318+Vikram-Jha@users.noreply.github.com>
2021-03-23 17:27:07 +0530

    Update DSA__4. Queue.java

--- 0. Data Structures/DSA__4. Queue.java
+++ 0. Data Structures/DSA__4. Queue.java
@@ -124,4 +124,57 @@ public class ArrayQueue {
   }
 }
 
+// Implementing  queue using stacks
+
+package com.company;
+
+import java.util.Stack;
+
+public class QueueTwoStack {
+    private Stack<Integer> stack1 = new Stack<>();
+    private Stack<Integer> stack2 = new Stack<>();
+
+    public void enqueue(int item){
+        stack1.push(item);
+    }
+
+    public int dequeue(){
+        if (isEmpty())
+            throw new IllegalStateException();
+
+        moveStack1ToStack2();
+
+        return stack2.pop();
+    }
+
+    public int peek() {
+        if (isEmpty())
+            throw new IllegalStateException();
+
+        moveStack1ToStack2();
+
+        return stack2.peek();
+    }
+
+
+    private void moveStack1ToStack2() {
+        if (stack2.isEmpty())
+            while (!stack1.isEmpty())
+                stack2.push(stack1.pop());
+    }
+
+    public boolean isEmpty() {
+        return stack1.isEmpty() && stack2.isEmpty();
+    }
+
+    @Override
+    public String toString(){
+        if(stack2.isEmpty())
+            moveStack1ToStack2();
+
+        return (stack2.toString());
+    }
+}
+
+
 
