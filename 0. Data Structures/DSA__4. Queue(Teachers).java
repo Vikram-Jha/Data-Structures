30b4e22473ab2440b851212a75c2eef026e8cca9
Vikram Jha <77636318+Vikram-Jha@users.noreply.github.com>
2021-03-24 17:24:09 +0530

    Update DSA__4. Queue(Teachers).java

--- 0. Data Structures/DSA__4. Queue(Teachers).java
+++ 0. Data Structures/DSA__4. Queue(Teachers).java
@@ -133,3 +133,173 @@ public static void reverse(Queue<Integer> queue, int k) {
             queue.add(queue.remove());
 
     }
+
+//Queue using Linkedlist in java
+
+package com.codewithmosh;
+
+import java.util.ArrayList;
+
+public class LinkedListQueue {
+    private class Node {
+        private int value;
+        private Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    private Node head;
+    private Node tail;
+    private int count;
+
+    // O(1)
+    public void enqueue(int item) {
+        var node = new Node(item);
+
+        if (isEmpty())
+            head = tail = node;
+        else {
+            tail.next = node;
+            tail = node;
+        }
+
+        count++;
+    }
+
+    // O(1)
+    public int dequeue() {
+        if (isEmpty())
+            throw new IllegalStateException();
+
+        int value;
+        if (head == tail) {
+            value = head.value;
+            head = tail = null;
+        }
+        else {
+            value = head.value;
+            var second = head.next;
+            head.next = null;
+            head = second;
+        }
+
+        count--;
+
+        return value;
+    }
+
+    // O(1)
+    public int peek() {
+        if (isEmpty())
+            throw new IllegalStateException();
+
+        return head.value;
+    }
+
+    // O(1)
+    public int size() {
+        return count;
+    }
+
+    // O(1)
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    // O(n)
+    public String toString() {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        Node current = head;
+        while (current != null) {
+            list.add(current.value);
+            current = current.next;
+        }
+
+        return list.toString();
+    }
+}
+
+//Stack with two queue
+
+package com.codewithmosh;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+// We always use queue1 for pushing new items.
+// When popping an item, we move all the items
+// except the last one from queue1 to queue2.
+//
+// Q1 [10, 20, 30, 40] => Q1 [40]
+// Q2 []               => Q2 [10, 20, 30]
+//
+// Now the item to be popped is in Q1. We remove
+// and return that as the item on top of the stack.
+//
+// Q1 []
+// Q2 [10, 20, 30]
+//
+// We should swap the queues, so we can repeat this
+// algorithm next time we need to pop the stack.
+//
+// Q1 [10, 20, 30]
+// Q2 []  (ready for moving items)
+
+public class StackWithTwoQueues {
+    private Queue<Integer> queue1 = new ArrayDeque<>();
+    private Queue<Integer> queue2 = new ArrayDeque<>();
+    private int top;
+
+    // O(1)
+    public void push(int item) {
+        queue1.add(item);
+        top = item;
+    }
+
+    // O(n)
+    public int pop() {
+        if (isEmpty())
+            throw new IllegalStateException();
+
+        while (queue1.size() > 1) {
+            top = queue1.remove();
+            queue2.add(top);
+        }
+
+        swapQueues();
+
+        return queue2.remove();
+    }
+
+    private void swapQueues() {
+        var temp = queue1;
+        queue1 = queue2;
+        queue2 = temp;
+    }
+
+    // O(1)
+    public boolean isEmpty() {
+        return queue1.isEmpty();
+    }
+
+    // O(1)
+    public int size() {
+        return queue1.size();
+    }
+
+    // O(1)
+    public int peek() {
+        if (isEmpty())
+            throw new IllegalStateException();
+
+        return top;
+    }
+
+    @Override
+    public String toString() {
+        return queue1.toString();
+    }
+}
