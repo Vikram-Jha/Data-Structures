9cef35042b82638b83e3ca0567398ca08e32208f
Vikram Jha <77636318+Vikram-Jha@users.noreply.github.com>
2021-03-24 18:40:32 +0530

    Update DSA__5. HashTable.java

--- 0. Data Structures/DSA__5. HashTable.java
+++ 0. Data Structures/DSA__5. HashTable.java
@@ -33,3 +33,70 @@ public static char findFirstNonRepeatingChar(String str) {
   }
 
 }
+
+//Implementing HashTable using an array of linklist
+
+package com.company;
+
+import java.util.LinkedList;
+
+public class HashTable {
+    private class Entry{
+        private int key;
+        private String value;
+
+        private Entry(int key, String value){
+            this.key = key;
+            this.value = value;
+        }
+
+        //[LL, LL, LL, LL, LL, LL, LL, LL] --> (K,V)object is stored by the linked list
+        private LinkedList<Entry>[] items = new LinkedList[5];
+
+        public void put(int key, String value) {
+            var index = hash(key);
+
+            if (items[index] == null)
+                items[index] = new LinkedList<>();
+
+            for (var entry : items[index]) {
+
+                if (entry.key == key)
+                    entry.value = value;
+                return;
+            }
+
+            items[index].addLast(new Entry(key, value));
+        }
+
+        public String get(int key){
+            var index = hash(key);
+            var bucket = items[index];
+            if(bucket!=null){
+                for(var entry : bucket)
+                    if(entry.key==key)
+                        return entry.value;
+            }
+            return null;
+        }
+
+        public void remove(int key) {
+            var index = hash(key);
+            var bucket = items[index];
+            if (bucket == null)
+                throw new IllegalArgumentException();
+
+            for (var entry : bucket) {
+                if (key == entry.key) {
+                    bucket.remove(entry);
+                    return;
+                }
+            }
+            throw new IllegalStateException();
+        }
+
+        private int hash(int key){
+            return key% items.length;
+        }
+    }
+}
